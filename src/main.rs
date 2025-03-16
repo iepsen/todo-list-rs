244045056599ef4cf14ed093160c42409144e417
Marcelo Iepsen <iepsen@gmail.com>
2025-03-16 14:59:05 +0100

    add first todo list implementation
    It completes the first step of building a todo list:
    https://gtk-rs.org/gtk4-rs/stable/latest/book/todo_1.html

--- src/main.rs
+++ src/main.rs
@@ -1,38 +1,31 @@
-mod custom_button;
+mod window;
+mod task_object;
+mod task_row;
 
-use gtk::{glib, Application, ApplicationWindow};
 use gtk::prelude::*;
-use crate::custom_button::CustomButton;
+use gtk::{gio, glib, Application};
+use window::Window;
 
 const APP_ID: &str = "com.github.iepsen.todo-list-rs";
 
 fn main() -> glib::ExitCode {
+    // Register and include resources
+    gio::resources_register_include!("templates.gresource")
+        .expect("Failed to register resources.");
+
     // Create a new application
     let app = Application::builder().application_id(APP_ID).build();
 
+    // Connect to "activate" signal of `app`
     app.connect_activate(build_ui);
 
     // Run the application
     app.run()
 }
 
+// ANCHOR: build_ui
 fn build_ui(app: &Application) {
-    // Create a button
-    let button = CustomButton::new();
-    button.set_margin_top(12);
-    button.set_margin_bottom(12);
-    button.set_margin_start(12);
-    button.set_margin_end(12);
-
-    // Create a window
-    let window = ApplicationWindow::builder()
-        .application(app)
-        .title("To-do List")
-        .child(&button)
-        .width_request(200)
-        .height_request(140)
-        .build();
-
-    // Present window
+    // Create new window and present it
+    let window = Window::new(app);
     window.present();
 }
